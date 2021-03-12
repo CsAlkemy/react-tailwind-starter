9068d31af90ca057004e34dee31f4eadb07b9492
CsAlkemy <alkemy48@gmail.com>
2021-03-12 11:27:59 +0600

    simple ui added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import "./App.css";
 
+
 function App() {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-5">
-      <div className="md:flex">
-        <div className="md:flex-shrink-0">
-          <img
-            className="h-48 w-full object-cover md:w-48"
-            src="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-            alt="Man looking at item at a store"
-          ></img>
-        </div>
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            Case study
-          </div>
-          <p className="mt-2 text-gray-500">
-            Getting a new business off the ground is a lot of hard work. Here
-            are five ideas you can use to find your first customers.
-          </p>
-        </div>
+    <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+    <img className='h-44 mb-6' src='https://cdn.worldvectorlogo.com/logos/tailwindcss.svg' alt="tailwindcsslooogoooo"/>
+    <div>
+      <h1 className='text-2xl md:text-4xl font-light my-10'>React TailwindCSS Starter</h1>
+      <div className='flex mx-auto items-center justify-center'>
+      <img src='https://cdn.worldvectorlogo.com/logos/tailwindcss.svg' className='h-10' alt='tailll'/>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png' className='h-12' alt='tailll'/>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Equal_symbol.svg/1200px-Equal_symbol.svg.png' className='h-12' alt='tailll'/>
+      <img src='https://cdn.iconscout.com/icon/free/png-256/cool-1642627-1393712.png' className='h-12' alt='tailll'/>
+      </div>
+      <div className='flex mx-auto items-center justify-center my-10'>
+        <a className='p-3 px-9 mr-3 bg-gradient-to-r from-blue-500 to-green-400   text-white rounded-md shadow-md' target='_blank' rel='noreferrer' href='https://github.com/CsAlkemy/react-tailwind-starter' >Download</a>
+        <a className='p-3 px-5 ml-3 bg-gradient-to-r from-blue-500 to-green-400 text-white rounded-md shadow-md' target='_blank' rel='noreferrer' href='https://github.com/CsAlkemy/react-tailwind-starter'> Buy me coffee</a>
       </div>
     </div>
+    </div>
+   
   );
 }
 
